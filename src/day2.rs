f4cd42a96b5558438d5ba29c50577da88fc3412f
Aron Heinecke <Ox0p54r36@t-online.de>
2022-12-02 14:34:38 +0100

    make it nightly rust and barely faster

--- src/day2.rs
+++ src/day2.rs
@@ -3,30 +3,83 @@ pub fn part1(input: &[u8]) -> i64 {
     let mut i = 0;
     let len = input.len();
     let mut sum = 0;
-    loop {
-        match i < len {
-            true => {
-                let a = unsafe {input.get_unchecked(i) - b'@'};
-                let b = unsafe {input.get_unchecked(i+2) - b'W'};
-                sum += match (a,b) {
-                    // draw
-                    (1,1) => 1+3,
-                    (2,2) => 2+3,
-                    (3,3) => 3+3,
-                    // win
-                    (3,1) => 1+6,
-                    (1,2) => 2+6,
-                    (2,3) => 3+6,
-                    // loss
-                    (2,1) => 1,
-                    (3,2) => 2,
-                    (1,3) => 3,
-                    _ => unreachable!()
-                };
-                i += 4;
-            },
-            false => break,
-        }
+    for data in input.array_chunks::<4>() {
+        let a = data[0] - b'@';
+        let b = data[2] - b'W';
+        sum += match (a,b) {
+            // draw
+            (1,1) => 1+3,
+            (2,2) => 2+3,
+            (3,3) => 3+3,
+            // win
+            (3,1) => 1+6,
+            (1,2) => 2+6,
+            (2,3) => 3+6,
+            // loss
+            (2,1) => 1,
+            (3,2) => 2,
+            (1,3) => 3,
+            _ => unreachable!()
+        };
+    }
+    if input.len() % 4 != 0 {
+        let a = input[input.len() -3] - b'@';
+        let b = input[input.len() -1] - b'W';
+        sum += match (a,b) {
+            // draw
+            (1,1) => 1+3,
+            (2,2) => 2+3,
+            (3,3) => 3+3,
+            // win
+            (3,1) => 1+6,
+            (1,2) => 2+6,
+            (2,3) => 3+6,
+            // loss
+            (2,1) => 1,
+            (3,2) => 2,
+            (1,3) => 3,
+            _ => unreachable!()
+        };
+    }
+    sum
+}
+
+#[aoc(day2, part2, Chars)]
+pub fn part2(input: &[u8]) -> i64 {
+    let mut i = 0;
+    let len = input.len();
+    let mut sum = 0;
+    for data in input.array_chunks::<4>() {
+        let a = data[0] - b'@';
+        let b = data[2] - b'W';
+        sum += match (a,b) {
+            (1,1) => 3,
+            (1,2) => 3+1,
+            (1,3) => 6+2,
+            (2,1) => 1,
+            (2,2) => 3+2,
+            (2,3) => 6+3,
+            (3,1) => 2,
+            (3,2) => 3+3,
+            (3,3) => 6+1,
+            _ => unreachable!()
+        };
+    }
+    if input.len() % 4 != 0 {
+        let a = input[input.len() -3] - b'@';
+        let b = input[input.len() -1] - b'W';
+        sum += match (a,b) {
+            (1,1) => 3,
+            (1,2) => 3+1,
+            (1,3) => 6+2,
+            (2,1) => 1,
+            (2,2) => 3+2,
+            (2,3) => 6+3,
+            (3,1) => 2,
+            (3,2) => 3+3,
+            (3,3) => 6+1,
+            _ => unreachable!()
+        };
     }
     sum
 }
