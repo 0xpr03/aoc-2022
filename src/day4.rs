033b75d50d55926a6cbe0c310419fdf682c409ed
Aron Heinecke <Ox0p54r36@t-online.de>
2022-12-04 19:14:30 +0100

    cleanup

--- src/day4.rs
+++ src/day4.rs
@@ -6,25 +6,9 @@ pub fn part1(input: &[u8]) -> i64 {
     .filter(|s| !s.is_empty())
     .map(|x|(x,x.iter().position(|x|*x == b',').unwrap()))
     .map(|(x,pos)|(&x[..pos],&x[pos+1..]))
-    // .map(|v|{
-    //     let (a,b) = v;
-    //     println!("a {} b {}",
-    //     std::str::from_utf8(a).unwrap(),
-    //     std::str::from_utf8(b).unwrap());
-    //     v
-    // })
     .map(|(a,b)|(a,a.iter().position(|x|*x == b'-').unwrap(),
     b,b.iter().position(|x|*x == b'-').unwrap()))
     .map(|(a,posa,b,posb)|(&a[..posa],&a[posa+1..],&b[..posb],&b[posb+1..]))
-    // .map(|v|{
-    //     let (a0,a1,b0,b1) = v;
-    //     println!("a1 {} a2 {} b1 {} b2 {}",
-    //     std::str::from_utf8(a0).unwrap(),
-    //     std::str::from_utf8(a1).unwrap(),
-    //     std::str::from_utf8(b0).unwrap(),
-    //     std::str::from_utf8(b1).unwrap());
-    //     v
-    // })
     .map(|(a0,a1,b0,b1)|(stou16(a0),stou16(a1),stou16(b0),stou16(b1)))
     .map(|v|contains(v.0,v.1,v.2,v.3)).sum()
 }
@@ -35,25 +19,9 @@ pub fn part2(input: &[u8]) -> i64 {
     .filter(|s| !s.is_empty())
     .map(|x|(x,x.iter().position(|x|*x == b',').unwrap()))
     .map(|(x,pos)|(&x[..pos],&x[pos+1..]))
-    // .map(|v|{
-    //     let (a,b) = v;
-    //     println!("a {} b {}",
-    //     std::str::from_utf8(a).unwrap(),
-    //     std::str::from_utf8(b).unwrap());
-    //     v
-    // })
     .map(|(a,b)|(a,a.iter().position(|x|*x == b'-').unwrap(),
     b,b.iter().position(|x|*x == b'-').unwrap()))
     .map(|(a,posa,b,posb)|(&a[..posa],&a[posa+1..],&b[..posb],&b[posb+1..]))
-    // .map(|v|{
-    //     let (a0,a1,b0,b1) = v;
-    //     println!("a1 {} a2 {} b1 {} b2 {}",
-    //     std::str::from_utf8(a0).unwrap(),
-    //     std::str::from_utf8(a1).unwrap(),
-    //     std::str::from_utf8(b0).unwrap(),
-    //     std::str::from_utf8(b1).unwrap());
-    //     v
-    // })
     .map(|(a0,a1,b0,b1)|(stou16(a0),stou16(a1),stou16(b0),stou16(b1)))
     .map(|v|overlaps(v.0,v.1,v.2,v.3)).sum()
 }
@@ -90,28 +58,9 @@ fn stou16(bytes: &[u8]) -> u16 {
     }
 }
 
-#[inline(always)]
-fn atoi(bytes: &[u8]) -> u32 {
-    match bytes.len() {
-        2 => (bytes[0] as u32) * 10
-            + (bytes[1] as u32) - 528,
-        1 => (bytes[0] - b'0') as u32,
-        v => unreachable!("{}",v),
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn test_atoi() {
-        assert_eq!(atoi(b"5"),5);
-        assert_eq!(atoi(b"10"),10);
-        assert_eq!(atoi(b"11"),11);
-        assert_eq!(atoi(b"99"),99);
-        assert_eq!(atoi(b"87"),87);
-    }
-
     #[test]
     fn test_contains() {
         assert_eq!(contains(2,4,4,5),0);
