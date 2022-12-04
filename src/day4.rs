1ca73d8f0c1961845709114031547e4211744697
Aron Heinecke <Ox0p54r36@t-online.de>
2022-12-04 19:11:53 +0100

    use u16 bytes and don't parse them anymore

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,5 @@
+use bytemuck::cast_slice;
+
 #[aoc(day4, part1, Chars)]
 pub fn part1(input: &[u8]) -> i64 {
     input.split(|x|*x == b'\n')
@@ -23,11 +25,8 @@ pub fn part1(input: &[u8]) -> i64 {
     //     std::str::from_utf8(b1).unwrap());
     //     v
     // })
-    .map(|(a0,a1,b0,b1)|(atoi(a0),atoi(a1),atoi(b0),atoi(b1)))
-    .map(|(a1,a2,b1,b2)|{
-        // println!("a1 {} a2 {} b1 {} b2 {}",a1,a2,b1,b2);
-        contains(a1,a2,b1,b2)
-    }).sum()
+    .map(|(a0,a1,b0,b1)|(stou16(a0),stou16(a1),stou16(b0),stou16(b1)))
+    .map(|v|contains(v.0,v.1,v.2,v.3)).sum()
 }
 
 #[aoc(day4, part2, Chars)]
@@ -55,15 +54,12 @@ pub fn part2(input: &[u8]) -> i64 {
     //     std::str::from_utf8(b1).unwrap());
     //     v
     // })
-    .map(|(a0,a1,b0,b1)|(atoi(a0),atoi(a1),atoi(b0),atoi(b1)))
-    .map(|(a1,a2,b1,b2)|{
-        // println!("a1 {} a2 {} b1 {} b2 {}",a1,a2,b1,b2);
-        overlaps(a1,a2,b1,b2)
-    }).sum()
+    .map(|(a0,a1,b0,b1)|(stou16(a0),stou16(a1),stou16(b0),stou16(b1)))
+    .map(|v|overlaps(v.0,v.1,v.2,v.3)).sum()
 }
 
 #[inline(always)]
-fn overlaps(a1: u32,a2: u32,b1: u32,b2: u32) -> i64 {
+fn overlaps(a1: u16,a2: u16,b1: u16,b2: u16) -> i64 {
     if a1 <= b1 && a2 >= b1 {
         1
     } else if b1 <= a2 && b2 >= a1 {
@@ -74,13 +70,23 @@ fn overlaps(a1: u32,a2: u32,b1: u32,b2: u32) -> i64 {
 }
 
 #[inline(always)]
-fn contains(a1: u32,a2: u32,b1: u32,b2: u32) -> i64 {
+fn contains(a1: u16,a2: u16,b1: u16,b2: u16) -> i64 {
     if a1 <= b1 && a2 >= b2 {
         1
     } else if a1 >= b1 && a2 <= b2 {
         1
     } else {
         0
+    }
+}
+
+#[inline(always)]
+fn stou16(bytes: &[u8]) -> u16 {
+    if bytes.len() > 1 {
+        let n: [u8;2] = bytes[0..2].try_into().unwrap();
+        u16::from_be_bytes(n)
+    } else {
+        bytes[0] as u16
     }
 }
 
