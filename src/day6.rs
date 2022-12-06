ec36f22db14f404fea6f8005748f135a2f2ce71d
Aron Heinecke <Ox0p54r36@t-online.de>
2022-12-06 21:32:32 +0100

    d6 optimize cases more

--- src/day6.rs
+++ src/day6.rs
@@ -24,12 +24,10 @@ pub fn part1(input: &[u8]) -> usize {
         // dbg!(index);
         for (i,v) in part[0..3].iter().enumerate().rev() {
             let skip_n = index + i;
-            if skip_n < skip {
+            if skip_n <= skip {
                 break;
-            } else if *v == part[3] {
-                if skip_n > skip {
-                    skip = index+i;
-                }
+            } if *v == part[3] {
+                skip = skip_n;
                 break;
             }
         }
@@ -64,12 +62,10 @@ pub fn part2(input: &[u8]) -> usize {
         // dbg!(index);
         for (i,v) in part[0..13].iter().enumerate().rev() {
             let skip_n = index + i;
-            if skip_n < skip {
+            if skip_n <= skip {
                 break;
             } else if *v == part[13] {
-                if skip_n > skip {
-                    skip = index+i;
-                }
+                skip = skip_n;
                 break;
             }
         }
