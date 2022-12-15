d33b746109db6c9b2e9f1dfdff980ecaef74bf73
Aron Heinecke <Ox0p54r36@t-online.de>
2022-12-15 22:30:18 +0100

    d15 p1 make it fast

--- src/day15.rs
+++ src/day15.rs
@@ -11,56 +11,105 @@ pub fn input_generator(input: &[u8]) -> Vec<u8> {
 }
 
 #[aoc(day15, part1, Schokis)]
-pub fn part1(input: &[u8]) -> usize {
+pub fn part1(input: &[u8]) -> isize {
     const ASKED_Y: isize = 2000000;
     // const ASKED_Y: isize = 10;
-    let mut min_x = isize::MAX;
-    let mut max_x = 0;
-    let sensors: Vec<_> = input.split(|x|*x == b'\n').filter(|v|!v.is_empty()).map(|v|{
-        let iter = v.split(|x|*x == b',' || *x == b':').map(trim_non_digit);
+    let mut ranges: Vec<Range> = Vec::with_capacity(25);
+    // let mut skipped = 0;
+    for v in input.split(|x|*x == b'\n').filter(|v|!v.is_empty()) {
+        let iter = v.split(|x|*x == b',' || *x == b':');
         // iter_next_chunk is marked as slow https://github.com/rust-lang/rust/issues/98326#issuecomment-1166338225
         // so we'll use this
         let data = iter.array_chunks::<4>().next().unwrap();
-        // for v in data {
-        //     print!("{}#",std::str::from_utf8(v).unwrap());
-        // }
-        // println!();
-        let s_x = atoi(data[0]);
-        let s_y = atoi(data[1]);
-        let b_x = atoi(data[2]);
-        let b_y = atoi(data[3]);
+
+        let s_x = atoi(&data[0][12..]);
+        let s_y = atoi(&data[1][3..]);
+        let b_x = atoi(&data[2][24..]);
+        let b_y = atoi(&data[3][3..]);
 
         let distance = s_x.abs_diff(b_x) + s_y.abs_diff(b_y);
         let distance_isize = distance as isize;
-        if s_x + distance_isize > max_x {
-            max_x = s_x + distance_isize;
+        if s_y > ASKED_Y && s_y - distance_isize > ASKED_Y {
+            continue;
         }
-        if s_x - distance_isize < min_x {
-            min_x = s_x - distance_isize;
+        if s_y < ASKED_Y && s_y + distance_isize < ASKED_Y {
+            continue;
         }
-        let s = Sensor {
-            x: s_x,
-            y: s_y,
-            bx: b_x,
-            by: b_y,
-            dist: distance,
-        };
-        // println!("{} {b_x},{b_y}",std::str::from_utf8(data[2]).unwrap());
-        // dbg!(&s);
-        s
-    }).collect();
-    (min_x..=max_x).filter(|x|{
-        sensors.iter().any(|s|s.x.abs_diff(*x)+s.y.abs_diff(ASKED_Y) <= s.dist)
-    }).count()-1
+        let rem = (distance - s_y.abs_diff(ASKED_Y)) as isize;
+        let mut range = Some(Range(s_x - (rem),s_x+(rem)));
+        
+        for rm in ranges.iter_mut() {
+            if let Some(r) = range {
+                range = r.adjust(rm);
+            } else {
+                break;
+            }
+        }
+        if let Some(r) = range {
+            ranges.push(r);
+        }
+    }
+    let mut new_ranges = Vec::with_capacity(ranges.len());
+    for p in ranges.into_iter() {
+        let mut range = Some(p);
+        
+        for rm in new_ranges.iter_mut() {
+            if let Some(r) = range {
+                range = r.adjust(rm);
+            } else {
+                break;
+            }
+        }
+        if let Some(r) = range {
+            new_ranges.push(r);
+        }
+    }
+    let mut new_ranges2 = Vec::with_capacity(new_ranges.len());
+    for p in new_ranges.into_iter() {
+        let mut range = Some(p);
+        
+        for rm in new_ranges2.iter_mut() {
+            if let Some(r) = range {
+                range = r.adjust(rm);
+            } else {
+                break;
+            }
+        }
+        if let Some(r) = range {
+            new_ranges2.push(r);
+        }
+    }
+    new_ranges2.iter().map(|x|x.count()).sum()
 }
 
-#[derive(Debug)]
-struct Sensor {
-    x: isize,
-    y: isize,
-    bx: isize,
-    by: isize,
-    dist: usize,
+
+#[derive(Debug,PartialEq, Eq, Copy, Clone)]
+struct Range(isize,isize);
+impl Range {
+    #[inline(always)]
+    fn adjust(self,other: &mut Range) -> Option<Range> {
+        if self.1 < other.0 || self.0 > other.1 {
+            Some(self)
+        } else if self.0 < other.0 {
+            if self.1 >= other.1 {
+                other.0 = self.0;
+                other.1 = self.1;
+                None
+            } else {
+                other.0 = self.0;
+                None
+            }
+        } else if self.1 > other.1 {
+            other.1 = self.1;
+            None
+        } else { // contained in other range
+            None
+        }
+    }
+    #[inline(always)]
+    fn count(&self) -> isize {
+        self.1 - self.0
+    }
 }
 
 #[inline(always)]
@@ -95,38 +144,52 @@ fn atoi(part: &[u8]) -> isize {
         (part[1] & 0xf) as isize *100+
         (part[2] & 0xf) as isize *10+
         (part[3] & 0xf) as isize) * mult
-    } else if part.len() == 3 {
-        ((part[0] & 0xf) as isize *100+
-        (part[1] & 0xf) as isize *10+
-        (part[2] & 0xf) as isize) * mult
-    } else if part.len() == 2 {
-        ((part[0] & 0xf) as isize *10+
-        (part[1] & 0xf) as isize) * mult
-    } else if part.len() == 1 {
-        ((part[0] & 0xf) as isize) * mult
+    // } else if part.len() == 3 {
+    //     ((part[0] & 0xf) as isize *100+
+    //     (part[1] & 0xf) as isize *10+
+    //     (part[2] & 0xf) as isize) * mult
+    // } else if part.len() == 2 {
+    //     ((part[0] & 0xf) as isize *10+
+    //     (part[1] & 0xf) as isize) * mult
+    // } else if part.len() == 1 {
+    //     ((part[0] & 0xf) as isize) * mult
     } else {
         unreachable!("Atoi Len: {}",part.len());
     }
 }
 
-#[inline(always)]
-fn trim_non_digit(data: &[u8]) -> &[u8] {
-    // taken from rusts trim_ascii
-    let mut bytes = data;
-    while let [first, rest @ ..] = bytes {
-        if (!first.is_ascii_digit()) && *first != b'-' {
-            bytes = rest;
-        } else {
-            break;
-        }
-    }
-    while let [rest  @ .., last] = bytes {
-        if (!last.is_ascii_digit()) && *last != b'-' {
-            bytes = rest;
-        } else {
-            break;
-        }
-    }
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    bytes
+    #[test]
+    fn test_range() {
+        // left intersect right from left
+        let mut r = Range(2,4);
+        assert_eq!(Range(1,3).adjust(&mut r),None);
+        assert_eq!(r,Range(1,4));
+        // left intersect right from left
+        let mut r = Range(1,3);
+        assert_eq!(Range(2,4).adjust(&mut r),None);
+        assert_eq!(r,Range(1,4));
+        // left contains right
+        let mut r = Range(2,3);
+        assert_eq!(Range(1,5).adjust(&mut r),None);
+        assert_eq!(r,Range(1,5));
+        // right contains left
+        let mut r = Range(1,5);
+        assert_eq!(Range(2,3).adjust(&mut r),None);
+        assert_eq!(r,Range(1,5));
+        // left doesn't intersect right
+        let mut r = Range(1,2);
+        assert_eq!(Range(3,4).adjust(&mut r),Some(Range(3,4)));
+        // left intersect right from left, barely
+        let mut r = Range(2,4);
+        assert_eq!(Range(1,2).adjust(&mut r),None);
+        assert_eq!(r,Range(1,4));
+        // left intersect right from right, barely
+        let mut r = Range(1,2);
+        assert_eq!(Range(2,4).adjust(&mut r),None);
+        assert_eq!(r,Range(1,4));
+    }
 }
